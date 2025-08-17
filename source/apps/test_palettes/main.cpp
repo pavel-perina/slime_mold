446b5f5c04be949b7957613d64797bdbbdabc535
Pavel Perina <pavel.perina@gmail.com>
2025-08-17 11:45:59 +0200

    cosmetic changes to palette test

--- source/apps/test_palettes/main.cpp
+++ source/apps/test_palettes/main.cpp
@@ -29,33 +29,27 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
-    const std::array<const char*, 5> cmapLabels = {
-        "RGB   ",
-        "CieLAB",
-        "OkLab ",
-        "CieLCH",
-        "OkLCH "
-    };
-    const std::array<color::GradientFunction, 5> cmapFuncs = {
-        color::gradientRgb,
-        color::gradientCieLab,
-        color::gradientOkLab,
-        color::gradientCieLch,
-        color::gradientOkLch
-    };
+    const std::array<std::pair<std::string, color::GradientFunction>, 5> gradFuncs ({
+        { "   RGB", color::gradientRgb    },
+        { "CieLAB", color::gradientCieLab },
+        { " OkLAB", color::gradientOkLab  },
+        { "CieLCH", color::gradientCieLch },
+        { " OkLCH", color::gradientOkLch  }
+    });
     const auto &palettes = presetPalettes();
+
     for (const auto& preset : palettes) {
-        std::println("{}:", preset.name);
+        std::println("\x1b[37;1m{}:\x1b[0m", preset.name);
         const auto& c0 = preset.palette[0];
         const auto& c1 = preset.palette[1];
         const auto& c2 = preset.palette[2];
-        for (int i = 0; i < cmapFuncs.size(); ++i) {
-            std::print("{} ", cmapLabels[i]);
-            auto cmap1 = cmapFuncs[i](c0, c1, 32);
+        for (const auto& [gradLabel, gradFn] : gradFuncs) {
+            std::print("\x1b[37;0m{}  ", gradLabel);
+            auto cmap1 = gradFn(c0, c1, 32);
             for (int i = 0; i < 32; ++i) {
                 std::print("{}\xE2\x96\x88", rgbToEscapeCode(cmap1[i]));
             }
-            auto cmap2 = cmapFuncs[i](c1, c2, 32);
+            auto cmap2 = gradFn(c1, c2, 32);
             for (int i = 1; i < 32; ++i) {
                 std::print("{}\xE2\x96\x88", rgbToEscapeCode(cmap2[i]));
             }
@@ -63,22 +57,22 @@ int main()
         }
     }
 
-    std::vector<std::tuple<std::string, color::Rgb, color::Rgb>> gradients = {
-        { "Blue to white", { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f, 1.0f } },
-        { "Blue to black", { 0.0f, 0.0f, 1.0f }, { 0.0f, 0.0f, 0.0f } },
-        { "Red to green",  { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
-        { "Red to blue",  { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } },
+    std::vector<std::tuple<std::string, color::Rgb, color::Rgb>> gradients ({
+        { "Blue to white",  { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f, 1.0f } },
+        { "Blue to black",  { 0.0f, 0.0f, 1.0f }, { 0.0f, 0.0f, 0.0f } },
+        { "Red to green",   { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
+        { "Red to blue",    { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f } },
         { "Blue to green",  { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f } },
-        { "Red to white", { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f, 1.0f } },
-        { "Purple to gold",  { 0.5f, 0.0f, 0.5f }, { 1.0f, 0.84f, 0.0f } },
-    };
-    for (const auto& gradient : gradients) {
-        std::println("{}:", std::get<0>(gradient));
-        for (int i = 0; i < cmapFuncs.size(); ++i) {
-            std::print("{} ", cmapLabels[i]);
-            auto cmap1 = cmapFuncs[i](std::get<1>(gradient), std::get<2>(gradient), 32);
+        { "Red to white",   { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f, 1.0f } },
+        { "Purple to gold", { 0.5f, 0.0f, 0.5f }, { 1.0f, .84f, 0.0f } },
+    });
+    for (const auto& [gradLabel, color1, color2] : gradients) {
+        std::println("\x1b[37;1m{}:\x1b[0m", gradLabel);
+        for (const auto& [gradFnLabel, gradFn] : gradFuncs) {
+            std::print("{} ", gradFnLabel);
+            auto cmap = gradFn(color1, color2, 32);
             for (int i = 0; i < 32; ++i) {
-                std::print("{}\xE2\x96\x88", rgbToEscapeCode(cmap1[i]));
+                std::print("{}\xE2\x96\x88", rgbToEscapeCode(cmap[i]));
             }
             std::println("\x1b[0m"); // reset color
         }
